e9b881b5593ee29fccd2905a87c39edd0ade057a
codyNog <yoneyoneyone99@gmail.com>
2022-06-18 12:05:29 +0900

    ビルドエラー解決

--- packages/shared/front/backend/index.ts
+++ packages/shared/front/backend/index.ts
@@ -1,11 +1,9 @@
 import { CategoryImpl } from "../repositories/Category";
 import { PostImpl } from "../repositories/Post";
-import { SeedImpl } from "../repositories/Seed";
 import { UserImpl } from "../repositories/User";
 
 export const backend = {
   user: UserImpl,
   post: PostImpl,
   category: CategoryImpl,
-  seed: SeedImpl,
 };
--- packages/shared/front/repositories/Seed/index.ts
+++
@@ -1,9 +0,0 @@
-import { v1Client } from "../../libs/aspida";
-
-const create = async (): Promise<void> => {
-  return await v1Client.seeds.$post();
-};
-
-export const SeedImpl = {
-  create,
-};
