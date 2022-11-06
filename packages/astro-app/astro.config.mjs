efd48441ce1640ad5244f42d615782e0e519217b
codyNog <yoneyoneyone99@gmail.com>
2022-11-07 00:48:17 +0900

    astro追加

--- packages/astro-app/astro.config.mjs
+++ packages/astro-app/astro.config.mjs
@@ -1,10 +1,7 @@
 import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
 
 // eslint-disable-next-line import/no-default-export
-export default {
-	// ...
-	integrations: [
-		// ...
-		react(),
-	],
-};
+export default defineConfig({
+  integrations: [react()],
+});
