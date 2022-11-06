d85f735bc3d41a4a74f5869a9355611649699f99
codyNog <yoneyoneyone99@gmail.com>
2022-11-07 00:48:26 +0900

    format

--- packages/astro-app/astro.config.mjs
+++ packages/astro-app/astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  integrations: [react()],
+	integrations: [react()],
 });
